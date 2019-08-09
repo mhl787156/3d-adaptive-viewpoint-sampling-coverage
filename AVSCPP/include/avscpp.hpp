197116ea956ddffcf3b58a798f36ff7787d05bf4
mickey li <mickeyhli@outlook.com>
2019-08-09 21:03:55 +0100

    Added Nearest neighbours clustering

--- AVSCPP/include/avscpp.hpp
+++ AVSCPP/include/avscpp.hpp
@@ -26,6 +26,18 @@
 #include <pcl/octree/octree_pointcloud_changedetector.h>
 #include <pcl/visualization/cloud_viewer.h>
 
+#include <pcl/ModelCoefficients.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+
 // Local Library Headers
 #include "render.hpp"
 #include "shader.hpp"
--- AVSCPP/src/avscpp.cpp
+++ AVSCPP/src/avscpp.cpp
@@ -173,8 +173,15 @@ void CoveragePlanner::sampleViewpoints(std::vector<GLfloat> boundingBox,
 }
 
 void CoveragePlanner::compareSeenpointsWithReference() {
+    // Filter Down seenPointCloud
+    pcl::VoxelGrid<pcl::PointXYZ> vg;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+    vg.setInputCloud (seenPointCloud);
+    vg.setLeafSize (0.2f, 0.2f, 0.2f); // Filtered to 10cm voxels
+    vg.filter (*cloud_filtered);
+
     // Compare the points we have seen with reference model
-    octree->setInputCloud(seenPointCloud);
+    octree->setInputCloud(cloud_filtered);
     octree->addPointsFromInputCloud();
 
     octree->switchBuffers();
@@ -188,12 +195,12 @@ void CoveragePlanner::compareSeenpointsWithReference() {
     // Get vector of points from current octree voxels which did not exist in previous buffer
     int numPoints = octree->getPointIndicesFromNewVoxels(missingPoints);
 
+    // Create new pointcloud to hold output
     pcl::PointCloud<pcl::PointXYZ>::Ptr tmppcl(new pcl::PointCloud<pcl::PointXYZ>);
     tmppcl->width = missingPoints.size();
     tmppcl->height = 1;
     tmppcl->points.resize(tmppcl->width);
 
-    // Print out output
     for(int i = 0; i < missingPoints.size(); i++) {
         auto point = objectPointCloud->points[missingPoints[i]];
         tmppcl->points[i].x = point.x;
@@ -201,17 +208,90 @@ void CoveragePlanner::compareSeenpointsWithReference() {
         tmppcl->points[i].z = point.z;
     }
 
-    printf("%i points were identified as missing\n", numPoints);
-    
+    printf("%i points were identified as missing out of %lu reference\n", numPoints, objectPointCloud->size());
 
-    pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
-    viewer.showCloud(tmppcl);
-    while(!viewer.wasStopped()) {}
-    viewer.~CloudViewer();
+    // pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
+    // viewer.showCloud(tmppcl);
+    // while(!viewer.wasStopped()) {}
+    // viewer.~CloudViewer();
 
 
-    // Switch back to seenPoints PointCloud on top
-    // octree->switchBuffers();
+    ///////
+    /////// Begin Clustering
+    ///////
+    cloud_filtered = tmppcl;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
+
+    // Create the segmentation object for the planar model and set all the parameters
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZ> ());
+    pcl::PCDWriter writer;
+    seg.setOptimizeCoefficients (true);
+    seg.setModelType (pcl::SACMODEL_PLANE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setMaxIterations (100);
+    seg.setDistanceThreshold (0.02);
+    int i=0, nr_points = (int) cloud_filtered->points.size ();
+    while (cloud_filtered->points.size () > 0.3 * nr_points)
+    {
+        // Segment the largest planar component from the remaining cloud
+        seg.setInputCloud (cloud_filtered);
+        seg.segment (*inliers, *coefficients);
+        if (inliers->indices.size () == 0)
+        {
+        std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+        break;
+        }
+
+        // Extract the planar inliers from the input cloud
+        pcl::ExtractIndices<pcl::PointXYZ> extract;
+        extract.setInputCloud (cloud_filtered);
+        extract.setIndices (inliers);
+        extract.setNegative (false);
+
+        // Get the points associated with the planar surface
+        extract.filter (*cloud_plane);
+        std::cout << "PointCloud representing the planar component: " << cloud_plane->points.size () << " data points." << std::endl;
+
+        // Remove the planar inliers, extract the rest
+        extract.setNegative (true);
+        extract.filter (*cloud_f);
+        *cloud_filtered = *cloud_f;
+    }
+    // Creating the KdTree object for the search method of the extraction
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+    tree->setInputCloud (cloud_filtered);
+
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+    ec.setClusterTolerance (1.0f); // 1m
+    ec.setMinClusterSize (50);
+    ec.setMaxClusterSize (25000);
+    ec.setSearchMethod (tree);
+    ec.setInputCloud (cloud_filtered);
+    ec.extract (cluster_indices);
+
+    int j = 0;
+    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+    {
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+        for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit) {
+            cloud_cluster->points.push_back (cloud_filtered->points[*pit]); //*
+        }
+        cloud_cluster->width = cloud_cluster->points.size ();
+        cloud_cluster->height = 1;
+        cloud_cluster->is_dense = true;
+
+        std::stringstream ss;
+        ss << "cloud_cluster_" << j << ".pcd";
+        writer.write<pcl::PointXYZ> (ss.str (), *cloud_cluster, false); //*
+        std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size () << " data points." << std::endl;
+        j++;
+    }
+
+    printf("-----\n");
 }
 
 void CoveragePlanner::calculateLKHTrajectories(std::vector<glm::vec3> initialPositions) {
